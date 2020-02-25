cede0a68ff2bbe4c51fc33ef49ce6c80bd9b1d84
risticivan12 <risticivan@yahoo.com>
2020-02-25 14:12:10 +0100

    izmena

--- src/main/java/com/mycompany/mavenproject1/Main.java
+++ src/main/java/com/mycompany/mavenproject1/Main.java
@@ -10,6 +10,9 @@ package com.mycompany.mavenproject1;
  * @author Stefan
  */
 public class Main {
+    public static int sabiranje (int a,int b) {
+        return a + b;
+    }
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello to you too");
